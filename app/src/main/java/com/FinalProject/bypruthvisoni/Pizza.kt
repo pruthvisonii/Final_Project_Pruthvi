b5a548e88781264eeb63752bd666a9684eacc682
sakshi <pruthvisoni0505@gmail.com>
2022-12-14 19:52:55 -0500

    Final Commit

---
+++ app/src/main/java/com/FinalProject/bypruthvisoni/Pizza.kt
@@ -0,0 +1,9 @@
+package com.FinalProject.bypruthvisoni
+
+class Pizza {
+    var pizza_size_price: Double = 0.0
+    var meat_price: Double = 0.0
+    var cheese_price: Double = 0.0
+    var veggies_price: Double = 0.0
+    var address: String = ""
+}
